28b8e1b3ef45e3037d0eed459139176a5d7092b6
zehra kaygısız <zehrakaygsz06@gmail.com>
2026-03-13 20:47:19 +0300

    MatLibrary

---
+++ ARRAY/k1.java
@@ -0,0 +1,17 @@
+package ARRAY;
+import java.math.*;
+import javax.sql.rowset.spi.SyncResolver;
+public class k1 {
+    public static void main(String[] args){
+    //MATH SINIFI METOTLARI KULLANIMI
+    System.out.println(Math.ceil (1.7));
+    System.out.println(Math.cos(30));
+    System.out.println(Math.floor (1.1));
+    System.out.println(Math.max(17,40));
+    System.out.println(Math.min(14,-4));
+    System.out.println(Math.pow(2,8));
+    System.out.println(Math.round(1.5));
+    System.out.println(Math.abs(-40));
+    System.out.println(Math.sqrt(16));
+}
+}
